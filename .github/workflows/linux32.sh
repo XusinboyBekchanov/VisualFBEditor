fc2f197474545e7dd1e1d57a0268d26a5b89fe87
Gray Programmerz <hackrefisher@gmail.com>
2021-07-30 21:31:09 +0500

    Update linux32.sh

--- .github/workflows/linux32.sh
+++ .github/workflows/linux32.sh
@@ -3,7 +3,7 @@ cd ..
 cd ..
 git clone https://github.com/XusinboyBekchanov/MyFbFramework
 apt install libgtk-3-dev
-wget -O FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/ubuntu-18.04/FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz
+wget -O FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/ubuntu-18.04/FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz
 tar xf FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz
 cd FreeBASIC-1.08.1-ubuntu-18.04-x86_64
 ./install.sh -i
