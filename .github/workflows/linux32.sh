25819a9590943bd925fa531fe77068b61a25a076
Gray Programmerz <hackrefisher@gmail.com>
2021-07-30 21:29:47 +0500

    Update linux32.sh

--- .github/workflows/linux32.sh
+++ .github/workflows/linux32.sh
@@ -3,9 +3,9 @@ cd ..
 cd ..
 git clone https://github.com/XusinboyBekchanov/MyFbFramework
 apt install libgtk-3-dev
-wget -O FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/ubuntu-18.04/FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz
-tar xf FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz
-cd FreeBASIC-1.08.1-ubuntu-18.04-x86
+wget -O FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/ubuntu-18.04/FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz
+tar xf FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz
+cd FreeBASIC-1.08.1-ubuntu-18.04-x86_64
 ./install.sh -i
 cd ..
 cd VisualFBEditor/src
