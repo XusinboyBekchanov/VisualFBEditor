13476ebdf1ed91f360986141689f05212c0decca
Xusinboy Bekchanov <bxusinboy@mail.ru>
2021-08-01 06:54:04 +0500

    Update linux32.sh

--- .github/workflows/linux32.sh
+++ .github/workflows/linux32.sh
@@ -2,9 +2,9 @@ cd ..
 cd ..
 cd ..
 apt install libgtk-3-dev
-wget -O FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/ubuntu-18.04/FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz
-tar xf FreeBASIC-1.08.1-ubuntu-18.04-x86.tar.xz
-cd FreeBASIC-1.08.1-ubuntu-18.04-x86
+wget -O FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/ubuntu-18.04/FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz
+tar xf FreeBASIC-1.08.1-ubuntu-18.04-x86_64.tar.xz
+cd FreeBASIC-1.08.1-ubuntu-18.04-x86_64
 ./install.sh -i
 cd ..
 cd VisualFBEditor
