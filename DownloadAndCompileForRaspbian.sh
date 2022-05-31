04a2e74929d4eed9591729c4c0f343c2ffa73b25
Xusinboy Bekchanov <bxusinboy@mail.ru>
2022-05-31 14:25:29 +0500

    Create DownloadAndCompileForRaspbian.sh

---
+++ DownloadAndCompileForRaspbian.sh
@@ -0,0 +1,20 @@
+sudo apt install gcc libncurses5-dev libffi-dev libgl1-mesa-dev libx11-dev libxext-dev libxrender-dev libxrandr-dev libxpm-dev libtinfo5 libgpm-dev
+sudo apt install libgtk-3-dev
+sudo apt install git
+sudo apt-get install wget
+rm -r VisualFBEditor
+git clone https://github.com/XusinboyBekchanov/VisualFBEditor
+cd VisualFBEditor
+git clone https://github.com/XusinboyBekchanov/MyFbFramework
+mkdir Compilers
+cd Compilers
+wget -O FreeBASIC-1.09.0-raspbian9-arm.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.09.0/Binaries-Linux/raspbian9/FreeBASIC-1.09.0-raspbian9-arm.tar.xz
+tar xf FreeBASIC-1.09.0-raspbian9-arm.tar.xz
+cd ..
+cd src
+../Compilers/FreeBASIC-1.09.0-raspbian9-arm/bin/fbc "VisualFBEditor.bas" -x "../VisualFBEditor_gtk3" -i "../MyFbFramework" -d __USE_GTK3__ -v
+cd ..
+cd MyFbFramework/mff
+../../Compilers/FreeBASIC-1.09.0-raspbian9-arm/bin/fbc -b "mff.bi" -dll -x "../libmff32_gtk3.so" -d __USE_GTK3__ -v
+cd ..
+cd ..
