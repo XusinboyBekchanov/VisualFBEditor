fb82d522e9ba79b7bdaf96fd63923ac9c29d9c28
Xusinboy Bekchanov <bxusinboy@mail.ru>
2022-08-26 08:46:52 +0500

    Fixed: DownloadAndCompile scripts on Linux

--- DownloadAndCompileForRaspbian.sh
+++ DownloadAndCompileForRaspbian.sh
@@ -6,6 +6,7 @@ rm -r VisualFBEditor
 git clone https://github.com/XusinboyBekchanov/VisualFBEditor
 cd VisualFBEditor/Controls
 git clone https://github.com/XusinboyBekchanov/MyFbFramework
+cd ..
 mkdir Compilers
 cd Compilers
 wget -O FreeBASIC-1.09.0-raspbian9-arm.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.09.0/Binaries-Linux/raspbian9/FreeBASIC-1.09.0-raspbian9-arm.tar.xz
@@ -14,7 +15,8 @@ cd ..
 cd src
 ../Compilers/FreeBASIC-1.09.0-raspbian9-arm/bin/fbc "VisualFBEditor.bas" -x "../VisualFBEditor_gtk3" -i "../Controls/MyFbFramework" -d __USE_GTK3__ -v
 cd ..
-cd MyFbFramework/mff
+cd Controls/MyFbFramework/mff
 ../../../Compilers/FreeBASIC-1.09.0-raspbian9-arm/bin/fbc -b "mff.bi" -dll -x "../libmff32_gtk3.so" -d __USE_GTK3__ -v
+cd ..
 cd ..
 cd ..
