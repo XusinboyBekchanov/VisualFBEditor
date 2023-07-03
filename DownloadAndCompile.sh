d0fb6719d002e6f9e3754b70f696b3634667115f
Xusinboy Bekchanov <bxusinboy@mail.ru>
2023-07-03 09:05:38 +0500

    Update DownloadAndCompile.sh

--- DownloadAndCompile.sh
+++ DownloadAndCompile.sh
@@ -8,14 +8,14 @@ git clone https://github.com/XusinboyBekchanov/MyFbFramework
 cd ..
 mkdir Compilers
 cd Compilers
-wget -O FreeBASIC-1.09.0-linux-x86_64.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.09.0/Binaries-Linux/FreeBASIC-1.09.0-linux-x86_64.tar.xz
-tar xf FreeBASIC-1.09.0-linux-x86_64.tar.xz
+wget -O FreeBASIC-1.10.0-linux-x86_64.tar.xz https://versaweb.dl.sourceforge.net/project/fbc/FreeBASIC-1.10.0/Binaries-Linux/FreeBASIC-1.10.0-linux-x86_64.tar.xz
+tar xf FreeBASIC-1.10.0-linux-x86_64.tar.xz
 cd ..
 cd src
-../Compilers/FreeBASIC-1.09.0-linux-x86_64/bin/fbc "VisualFBEditor.bas" -x "../VisualFBEditor64_gtk3" -i "../Controls/MyFbFramework" -d __USE_GTK3__ -v
+../Compilers/FreeBASIC-1.10.0-linux-x86_64/bin/fbc "VisualFBEditor.bas" -x "../VisualFBEditor64_gtk3" -i "../Controls/MyFbFramework" -d __USE_GTK3__ -v
 cd ..
 cd Controls/MyFbFramework/mff
-../../../Compilers/FreeBASIC-1.09.0-linux-x86_64/bin/fbc -b "mff.bi" -dll -x "../libmff64_gtk3.so" -d __USE_GTK3__ -v
+../../../Compilers/FreeBASIC-1.10.0-linux-x86_64/bin/fbc -b "mff.bi" -dll -x "../libmff64_gtk3.so" -d __USE_GTK3__ -v
 cd ..
 cd ..
 cd ..
