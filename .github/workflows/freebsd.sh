0a07204a1211c1861391134b485c62b3d06b97ed
Xusinboy Bekchanov <bxusinboy@mail.ru>
2025-03-08 18:15:35 +0500

    freebsd

---
+++ .github/workflows/freebsd.sh
@@ -0,0 +1,34 @@
+cd ..
+cd ..
+cd ..
+apt install libgtk-3-dev
+
+wget -O FreeBASIC-1.10.1-freebsd-x86_64.tar.gz https://sourceforge.net/projects/fbc/files/FreeBASIC-1.10.1/Binaries-FreeBSD/FreeBASIC-1.10.1-freebsd-x86_64.tar.gz
+tar xf FreeBASIC-1.10.1-freebsd-x86_64.tar.gz
+cd FreeBASIC-1.10.1-freebsd-x86_64
+./install.sh -i
+cd ..
+cd VisualFBEditor
+git clone https://github.com/XusinboyBekchanov/MyFbFramework
+cd src
+fbc "VisualFBEditor.bas" -x "../VisualFBEditor64_gtk3" -i "../MyFbFramework" -d __USE_GTK3__ -v
+cd ..
+
+if [ ! -f VisualFBEditor64_gtk3 ]
+then
+    echo "VisualFBEditor64_gtk3 does not exist"
+    exit 1
+fi
+
+cd MyFbFramework/mff
+fbc -b "mff.bi" -dll -x "../libmff64_gtk3.so" -d __USE_GTK3__ -v
+
+if [ ! -f ../libmff64_gtk3.so ]
+then
+    echo "libmff64_gtk3.so does not exist"
+    exit 1
+fi
+
+cd ..
+cd ..
+ls
